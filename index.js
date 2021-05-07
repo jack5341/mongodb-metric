5cfa29fc4163cb3e2e315afd5e99ecb57c3b4e88
Nedim <nedim.akar53411@gmail.com>
2021-05-07 13:35:18 +0200

    now api get all data from url

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const app = express();
 
 app.get("/", async (req, res) => {
   const queryDBString = req.query.string;
+  const dbName = req.query.db
   const MongoClient = require("mongodb").MongoClient;
   const client = new MongoClient(queryDBString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
   client.connect(async () => {
-    const collection = await client.db("sample_airbnb").stats();
-    client.close();
+    const collection = await client.db(dbName).stats();
+    res.json({
+      status: 'succes',
+      data: collection
+    })
+    client.close()
   });
-  res.end();
 });
 
 const PORT = process.env.PORT || 3000;
