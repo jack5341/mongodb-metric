cdea98c11a3b0f2d6a0aaf21e96c79a3965018f1
Nedim <nedim.akar53411@gmail.com>
2021-05-07 21:04:46 +0200

    added inputs for string and db name

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-
-dotenv.config();
-
+const path = require("path");
 const app = express();
 
+dotenv.config();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
 app.get("/", async (req, res) => {
-  const queryDBString = req.query.string;
-  const dbName = req.query.db
+  res.render("index")
+})
+
+app.post("/", (req,res) => {
   const MongoClient = require("mongodb").MongoClient;
-  const client = new MongoClient(queryDBString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  client.connect(async () => {
-    const collection = await client.db(dbName).stats();
-    res.json({
-      status: 'succes',
-      data: collection
-    })
-    client.close()
-  });
-});
+  const queryDBString = req.body.db_string;
+  const dbName = req.body.db_name;
+  
+  if (queryDBString && dbName) {
+    const client = new MongoClient(
+      queryDBString,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    client.connect(async () => {
+      const collection = await client.db(dbName).stats();
+      res.json({
+        status: "succes",
+        data: collection,
+      });
+      client.close();
+    });
+  }
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
