059de963d799476c94aec92a630a4f6e216c90b4
Nedim <nedim.akar53411@gmail.com>
2021-05-14 23:19:33 +0200

    metric page is complated

--- pages/metrics.js
+++ pages/metrics.js
@@ -1,11 +1,61 @@
 import jwt from "jsonwebtoken";
 import { useRouter } from "next/router";
+import { Bar } from "react-chartjs-2";
 
 export default function Metrics() {
   const router = useRouter();
 
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
   if (router.query.token) {
-    return <h1> {JSON.stringify(jwt.decode(router.query.token))} </h1>;
+    const obj = jwt.decode(router.query.token);
+    const labels = Object.keys(obj)
+    const values = Object.values(obj)
+    const data = {
+      labels: labels.slice(5,8),
+      datasets: [
+        {
+          label: "Byte",
+          data: values.slice(5,8),
+          backgroundColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ]
+        },
+      ],
+    };
+
+    return (
+      <>
+        <h1 className="text-center text-3xl text-bold uppercase mt-5">
+          {" "}
+          {obj.db}{" "}
+        </h1>
+        <Bar className="p-20 pt-5 pb-0" data={data} options={options} />
+      </>
+    );
   }
   return null;
 }
